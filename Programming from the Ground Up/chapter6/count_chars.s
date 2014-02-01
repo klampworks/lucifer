32c3b81a8f74b31320af165f96edf75f8296d736
yukipyon <wolfvbear@gmail.com>
2014-02-01 15:58:04 +0000

    Added function to count chars.

---
+++ Programming from the Ground Up/chapter6/count_chars.s
@@ -0,0 +1,23 @@
+.type count_chars, @function
+.globl count_chars
+
+count_chars:
+pushl %ebp
+movl %esp, %ebp
+movl $0, %ecx
+
+movl 8(%ebp), %edx
+
+count_loop_begin:
+	movb (%edx), %al
+	cmpb, $0, %al
+	je count_loop_end
+
+	incl %ecx,
+	incl %edx,
+	jmp count_loop_begin
+
+cound_loop_end:
+	movl %ecx, %eax
+	popl %ebp
+	ret

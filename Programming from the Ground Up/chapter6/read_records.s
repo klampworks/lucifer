92faa5d97b4d4a764614dc2b6b739cd0c491e438
yukipyon <wolfvbear@gmail.com>
2014-02-02 13:31:41 +0000

    Fixed incorrect syscall numbers.

--- Programming from the Ground Up/chapter6/read_records.s
+++ Programming from the Ground Up/chapter6/read_records.s
@@ -15,7 +15,7 @@ _start:
 movl %esp, %ebp
 subl $8, %esp
 
-movl $3, %eax
+movl $5, %eax
 movl $file_name, %ebx
 movl $0, %ecx 	#Readonly
 movl $0666, %edx
@@ -50,6 +50,6 @@ record_read_loop:
 	jmp record_read_loop
 
 finished_reading:
-	movl $0, %eax
+	movl $1, %eax
 	movl $0, %ebx
 	int $0x80

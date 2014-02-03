901baf892ccdb790c0549ed977f50354922a9715
yukipyon <wolfvbear@gmail.com>
2014-02-03 19:25:16 +0000

    Mergesort.

---
+++ MIT Introduction to Algorithms/mergesort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <assert.h>
+
+void mergsort(int *input, int len, int(*cmp)(int a, int b))
+{
+	if (len < 2)
+		return;
+
+	for (int j = 1; j < len; ++j) {
+
+		int key = input[j];
+		int i = j - 1;
+
+		while (i >= 0 && cmp(input[i], key)) {
+
+			input[i+1] = input[i];
+			--i;
+		}
+
+		input[i+1] = key;
+	}
+}
+
+void print_array(int *input, int len)
+{
+	for (int i = 0; i < len; i++)
+		printf("%d\n", input[i]);
+}
+
+int greater(int a, int b)
+{
+	return a >= b;
+}
+
+int lesser(int a, int b)
+{
+	return a <= b;
+}
+
+void verify(int *input, int len, int(*cmp)(int a, int b))
+{
+	if (len < 2)
+		return;
+
+	for (int i = 2; i < len; ++i)
+		assert(cmp(input[i], input[i-1]));
+}
+
+int main()
+{
+
+	int input[] = {31, 41, 59, 26, 41, 58};
+	int length = sizeof input / sizeof(input[0]);
+
+	mergsort(input, length, &lesser);
+	print_array(input, length);
+	verify(input, length, &lesser);
+}

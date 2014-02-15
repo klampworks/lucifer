a0c8e02f1edf4d7fe6a72fc8f9e29987d4442d92
yukipyon <wolfvbear@gmail.com>
2014-02-15 12:34:51 +0000

    Reformatting.

--- Programming from the Ground Up/chapter6/write_newline.s
+++ Programming from the Ground Up/chapter6/write_newline.s
@@ -8,15 +8,16 @@ newline:
 .section .text
 
 write_newline:
-pushl %ebp
-movl %esp, %ebp
+	pushl %ebp
+	movl %esp, %ebp
 
-movl $4, %eax
-movl 8(%ebp), %ebx
-movl $newline, %ecx
-movl $1, %edx
-int $0x80
-movl %ebp, %esp
-popl %ebp
-ret
+	movl $4, %eax
+	movl 8(%ebp), %ebx
+	movl $newline, %ecx
+	movl $1, %edx
+	int $0x80
+
+	movl %ebp, %esp
+	popl %ebp
+	ret
 

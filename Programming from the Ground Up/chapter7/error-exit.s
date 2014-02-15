e0ffdab4f74da891474c262c3977781da371c791
yukipyon <wolfvbear@gmail.com>
2014-02-15 12:17:02 +0000

    Function for printing an error and quitting.

---
+++ Programming from the Ground Up/chapter7/error-exit.s
@@ -0,0 +1,37 @@
+.include "../chapter6/linux.s"
+.globl error_exit
+.type error_exit, @function
+
+error_exit:
+	pushl %ebp
+	movl $esp, %ebp
+
+	# Error code, first parameter.
+	movl 8(%ebp), %ecx
+
+	pushl %ecx
+	call count_chars
+	popl %ecx
+
+	movl %eax, %edx
+	movl $3, %ebx
+	movl $4, %eax
+	int $0x80
+
+	movl 12(%ebp), %ecx
+
+	pushl %ecx
+	call count_chars
+	popl %ecx
+
+	movl %eax, %edx
+	movl $3, %ebx
+	movl $4, %eax
+	int $0x80
+
+	pushl $3
+	call write_newline
+
+	movl $1, %eax
+	movl $1, %ebx
+	int $0x80

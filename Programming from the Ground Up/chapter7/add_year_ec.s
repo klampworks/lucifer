5354492cb4cd89bb21101c83f37bda6a9dd212dd
yukipyon <wolfvbear@gmail.com>
2014-02-15 12:26:29 +0000

    Added error handling to add_year.

---
+++ Programming from the Ground Up/chapter7/add_year_ec.s
@@ -0,0 +1,73 @@
+.include "linux.s"
+.include "record_def.s"
+
+.section .data
+input_file_name:
+.ascii "test.dat\0"
+
+output_file_name:
+.ascii "testout.dat\0"
+
+.section .bss
+.lcomm record_buffer, RECORD_SIZE
+
+.section .text
+.globl _start
+
+_start:
+	movl %esp, %ebp
+	subl $8, %esp
+
+	movl $5, %eax
+	movl $input_file_name, %ebx
+	movl $0, %ecx
+	movl $0666, %edx
+	int $0x80
+
+	movl %eax, -4(%ebp)
+
+	cmpl $0, %eax
+	jl continue_processing
+
+	.section .data
+	no_open_file_code:
+	.ascii "0001: \0"
+	no_open_file_msg:
+	.ascii "Can't open input file\0"
+
+	.section .text
+	pushl $no_open_file_msg
+	pushl $no_open_file_code
+	call error_exit
+
+	continue_processing:
+	movl $5, %eax
+	movl $output_file_name, %ebx
+	movl $0101, %ecx
+	movl $0666, %edx
+	int $0x80
+
+	movl %eax, -8(%ebp)
+
+	loop_begin:
+		pushl -4(%ebp)
+		pushl $record_buffer
+		call read_record
+		addl $8, %esp
+
+		cmpl $RECORD_SIZE,%eax
+		jne loop_end
+
+		incl record_buffer + RECORD_AGE
+
+		pushl -8(%ebp)
+		pushl $record_buffer
+		call write_record
+		addl $8, %esp
+
+		jmp loop_begin
+
+	loop_end:
+		movl $1, %eax
+		movl $0, %ebx
+		int $0x80

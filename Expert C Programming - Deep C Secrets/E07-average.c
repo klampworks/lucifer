033831e11e8142bcdf52620ba9df887a3d194bc6
yukipyon <wolfvbear@gmail.com>
2014-02-22 17:17:06 +0000

    Exercise 7.

---
+++ Expert C Programming - Deep C Secrets/E07-average.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+void gen_numbers()
+{
+	srand(time(NULL));
+	FILE *fp = fopen("numbers", "w");
+
+	for (int i = 0; i < 10000; i++) {
+		fprintf(fp, "%d\n", rand() % 100);
+	}
+
+	fclose(fp);
+}
+
+int main()
+{
+	assert(sizeof(long long) == 8);
+	gen_numbers();
+	FILE *fp = fopen("numbers", "r");
+
+	const int maxbuf = 19;
+	char buf[maxbuf];
+	int pos = 0;
+
+	unsigned long long total = 0;
+	unsigned long long acc = 0;
+	int count = 0;
+
+	char ch;
+	while ((ch =  getc(fp))) {
+
+		if (ch == EOF)
+			break;
+
+		if (ch != '\n') {
+
+			buf[pos] = ch;	
+			++pos;
+
+			if (pos == maxbuf) {
+				printf("Encountered excessivly long number.\n");
+				exit(1);
+			}
+		} else {
+
+			buf[pos] = '\0';
+			acc += atoi(buf);
+			count++;
+
+			/* If the most signifigant bit is set then we risk an overflow if we continue,
+			 * calculate the current average and add it to the total. */
+			if (acc & 0x20000) {
+				total += (acc / count);
+				acc = count = 0;
+			}
+
+			pos = 0;
+		}
+	}
+
+	fclose(fp);
+	if (count)
+		total += (acc / count);
+
+	printf("Average is %lld\n", total);
+}

5e16a691745a83d3aad881df4bcc491a3bc512bc
yukipyon <wolfvbear@gmail.com>
2014-02-24 19:47:56 +0000

    Exercise 3.

--- Expert C Programming - Deep C Secrets/E03-segments_of_an_executable.c
+++ Expert C Programming - Deep C Secrets/E03-segments_of_an_executable.c
@@ -1,3 +1,27 @@
+/*
+ * Exercise 03: Look at the Segments in an Executable
+ *
+ * 1. Compile the "hello world" program, run ls -l on the executable to get 
+ * its overall size, and run size to get the sizes of the segments within it.
+ *
+ * 2. Add the declaration of a global array of 1000 ints, recompile, and 
+ * repeat the measurements. Notice the differences.
+ *
+ * 3. Now add an initial value in the declaration of the array (remember, 
+ * C doesn't force you to provide a value for every element of an array in 
+ * an initializer). This will move the array from the BSS segment to the data 
+ * segment. Repeat the measurements. Notice the differences.
+ *
+ * 4. Now add the declaration of a big array local to a function. Declare a 
+ * second big local array with an initializer. Repeat the measurements. Is 
+ * data defined locally inside a function stored in the executable? Does it 
+ * make any difference if it's initialized or not?
+ *
+ * 5. What changes occur to file and segment sizes if you compile for 
+ * debugging? For maximum optimization?
+ *
+ */
+
 #include <stdio.h>
 
 /*

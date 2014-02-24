ac99c14e0abc1b1c5ecc10275eb600584074785b
yukipyon <wolfvbear@gmail.com>
2014-02-24 19:48:10 +0000

    Exercise 5.

--- Expert C Programming - Deep C Secrets/E05-the_stack_frame.c
+++ Expert C Programming - Deep C Secrets/E05-the_stack_frame.c
@@ -1,3 +1,17 @@
+/*
+ * Exercise 05: The Stack Frame
+ *
+ * 1. Manually trace the flow of control in the above program, and fill in 
+ * the stack frames at each call statement. For each return address, use the 
+ * line number to which it will go back.
+ * 
+ * 2. Compile the program for real, and run it under the debugger. Look at the 
+ * additions to the stack when a function has been called. Compare with your 
+ * desk checked results to work out exactly what a stack frame looks like on 
+ * your system.
+ *
+ */
+
 #include <stdio.h>
 
 void a (int i) {

b60bc47284dff9045d89a9be6aab21d576c934db
yukipyon <wolfvbear@gmail.com>
2014-02-25 19:39:17 +0000

    Exercise 11.

--- Expert C Programming - Deep C Secrets/E11-banana_peel.c
+++ Expert C Programming - Deep C Secrets/E11-banana_peel.c
@@ -1,3 +1,20 @@
+/*
+ * Exercise 11: How to Fake Out Prototypes
+ *
+ * Try a few examples to clarify the issues here. Create the following 
+ * function in a file of its own.
+ *
+ * Try calling it with and without a prototype, and with a prototype that 
+ * doesn't match the definition.
+ * In each case, predict what will happen before trying it. Check your 
+ * prediction by writing a union that allows you to store a value of one 
+ * type, and retrieve another of a different size. Does changing the order of 
+ * the parameters (in the declaration and the definition) affect how the 
+ * values are perceived in the called function? Account for this. How many 
+ * of the error cases does your compiler catch?
+ *
+ */
+
 #include <stdio.h>
 #include "E11-banana_peel.h"
 

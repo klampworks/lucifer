c12ed353ed60eba5bd10c54e0ab07486a44abff6
yukipyon <wolfvbear@gmail.com>
2014-02-25 19:37:14 +0000

    Exercise 10.

--- Expert C Programming - Deep C Secrets/E10-test_crashing.c
+++ Expert C Programming - Deep C Secrets/E10-test_crashing.c
@@ -1,3 +1,14 @@
+/*
+ * Exercise 10: Test Crash Your Software
+ *
+ * Complete the test program fragments above.
+ * Try running them to see how these bugs are reported by the OS.
+ * Extra Credit: Write signal handlers to catch the bus error and 
+ * segmentationHave them print a more user-friendly message, and exit.
+ * Rerun your program.
+ *
+ */
+
 #include <stdio.h>
 
 void bus_error()

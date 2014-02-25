dcd1448a2ce7930830dfa559cd3411cfc0220ffc
yukipyon <wolfvbear@gmail.com>
2014-02-25 18:35:22 +0000

    Exercise 1.

---
+++ The C++ Programming Language Special Edition (2000)/chapter 1-4/exercise1.cpp
@@ -0,0 +1,14 @@
+/*
+ * Exercise 1.
+ *
+ * Get the "Hello, world!" program (§3.2) to run. If that program doesn't 
+ * compile as writ-ten, look at §B.3.1.
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world!\n";
+}

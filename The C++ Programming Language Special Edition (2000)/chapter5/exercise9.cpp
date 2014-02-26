40b21c3609de04196f5a286e003a60b2617030c7
yukipyon <wolfvbear@gmail.com>
2014-02-26 19:52:26 +0000

    Exercise 9.

---
+++ The C++ Programming Language Special Edition (2000)/chapter5/exercise9.cpp
@@ -0,0 +1,15 @@
+/*
+ * Exercise 9.
+ *
+ * Find an example where it would make sense to use a name in its own 
+ * initializer.
+ *
+ */
+
+/* Linked list. */
+struct node {
+	void *data;
+	struct node *child;
+};
+
+int main(){}

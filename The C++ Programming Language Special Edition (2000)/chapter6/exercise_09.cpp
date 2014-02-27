28b42e03caaa8f8c02a39b667fb2843eb5073377
yukipyon <wolfvbear@gmail.com>
2014-02-27 19:20:45 +0000

    Exercise 9.

---
+++ The C++ Programming Language Special Edition (2000)/chapter6/exercise_09.cpp
@@ -0,0 +1,19 @@
+/*
+ * Excerise 9.
+ *
+ * Fully parenthesize the following expressions:
+ *   *p++
+ *   *--p
+ *   ++a--
+ *   (int* )p->m
+ *   *p.m
+ *   *a [i]
+ *
+ *   *(p++)
+ *   *--p	???
+ *   ++(a--)
+ *   (int* )(p->m)
+ *   *(p.m)
+ *   *(a [i])
+ *
+ */

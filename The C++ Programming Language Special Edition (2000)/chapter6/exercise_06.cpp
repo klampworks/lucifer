371e6cd758be58b839b3acf489f6d78ab07504ca
yukipyon <wolfvbear@gmail.com>
2014-02-27 19:16:01 +0000

    Exercise 6.

---
+++ The C++ Programming Language Special Edition (2000)/chapter6/exercise_06.cpp
@@ -0,0 +1,22 @@
+/*
+ * Exercise 6.
+ *
+ * Find 5
+ * different c++ constructs for which the meaning is implementation-
+ * defined (§C.2).
+ *
+ */
+
+#if 0
+
+sizeof(int);
+
+-1 >> 1;
+
+i++ * i++;
+
+INT_MAX;
+
+if (i++, --i);
+
+#endif

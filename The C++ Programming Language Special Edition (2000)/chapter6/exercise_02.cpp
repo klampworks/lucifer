403cb543e77361565a9cd8e727a523ee35847efd
yukipyon <wolfvbear@gmail.com>
2014-02-27 18:47:08 +0000

    Exercise 2.

---
+++ The C++ Programming Language Special Edition (2000)/chapter6/exercise_02.cpp
@@ -0,0 +1,28 @@
+/*
+ * Exercise 2.
+ *
+ * Fully parenthesize the following expressions:
+ *
+ * a = b + c * d << 2 & 8
+ * a & 077 != 3
+ * a == b || a == c && c < 5
+ * c=x !=O
+ * 0<= i < 7
+ * f(1,2)+3
+ * a = b == c ++
+ * a=b=c=O
+ * a [4] [2] *= * b ? c : * d * 2
+ * a-b,c=d
+ *
+ * a = ((b + (c * d << 2) & 8
+ * a & (077 != 3)
+ * ((a == b) || (a == c)) && (c < 5)
+ * c=(x !=O)
+ * (0<= i) < 7
+ * (f(1,2))+3
+ * a = (b == (c ++))
+ * a=(b=(c=O))
+ * (a [4] [2]) *= (* b) ? c : ((* d) * 2)
+ * (a-b),(c=d)
+ */
+

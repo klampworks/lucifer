37186096983c0808a3b0074a5a077c9425580f49
yukipyon <wolfvbear@gmail.com>
2014-02-28 19:37:09 +0000

    Exercise 16.

--- Expert C Programming - Deep C Secrets/E16-bit-patterns.c
+++ Expert C Programming - Deep C Secrets/E16-bit-patterns.c
@@ -1,3 +1,11 @@
+/*
+ * Exercise 16: Check Those Bit Patterns
+ *
+ * Write a one-liner to check whether or not the bit pattern for floating 
+ * point 0.0 is the same as integer zero on your system.
+ *
+ */
+
 #include <assert.h>
 
 union u {

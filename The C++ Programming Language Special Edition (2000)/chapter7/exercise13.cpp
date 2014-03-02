c4c1b08a36b23aea86f2de041c8ec67bc6e77d3d
yukipyon <wolfvbear@gmail.com>
2014-03-02 17:10:38 +0000

    Exercise 13.

---
+++ The C++ Programming Language Special Edition (2000)/chapter7/exercise13.cpp
@@ -0,0 +1,17 @@
+/*
+ * Exercise 13.
+ *
+ * Look at some programs to get an idea of the diversity of styles of names 
+ * actually used.
+ * How are uppercase letters used? How is the underscore used? When are short 
+ * names such as i and x used?
+ *
+ */
+
+#define CONSTANT "isupper"
+
+int main()
+{
+	int my_int;
+	for (int i = 0; false;);
+}

ad35d9adb268a3f011eaa94f0f748451fb1e959e
yukipyon <wolfvbear@gmail.com>
2014-03-02 14:57:31 +0000

    Exercise 5.

---
+++ The C++ Programming Language Special Edition (2000)/chapter7/exercise05.cpp
@@ -0,0 +1,11 @@
+/*
+ * Exercise 5.
+ *
+ * Convert a small C program to C++. Modify the header files to declare all 
+ * functions called and to declare the type of every argument. Where possible, 
+ * replace #defines with enum, const, or inline. Remove extern declarations 
+ * from. c files and if necessary convert all function defi-nitions to C++ 
+ * function definition syntax. Replace calls of malloc () and/ree () with new 
+ * and delete. Remove unnecessary casts.
+ *
+ */

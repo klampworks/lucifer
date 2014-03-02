3db8b839b4ba90fb25b74241907355fc9b0d29f8
yukipyon <wolfvbear@gmail.com>
2014-03-02 16:50:30 +0000

    Exercise 10.

---
+++ The C++ Programming Language Special Edition (2000)/chapter7/exercise10.cpp
@@ -0,0 +1,8 @@
+/*
+ * Exercise 10.
+ *
+ * Write a program to help decipher messages encrypted with the method 
+ * described in §7.10[9] without knowing the key. Hint: See David Kahn: The 
+ * Codebreakers, Macmillan, 1967, New York, pp. 207-213.
+ *
+ */

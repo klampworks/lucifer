f89ddbc05ad0891740ab607afa77637a1de7e9b5
yukipyon <wolfvbear@gmail.com>
2014-03-04 19:23:52 +0000

    Exercise 6.

---
+++ The C++ Programming Language Special Edition (2000)/chapter9/exercise06.cpp
@@ -0,0 +1,8 @@
+/*
+ * Exercise 6.
+ *
+ *  How is dynamic linking achieved on your system? What restrictions are 
+ *  placed on dynam-ically linked code? What requirements are placed on code 
+ *  for it to be dynamically linked?
+ *
+ */

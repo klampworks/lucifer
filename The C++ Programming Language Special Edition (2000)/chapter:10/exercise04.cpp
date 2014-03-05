163e3569e98f926f5286bacc395507565b1aa2e1
yukipyon <wolfvbear@gmail.com>
2014-03-05 18:11:47 +0000

    Exercise 4.

---
+++ The C++ Programming Language Special Edition (2000)/chapter:10/exercise04.cpp
@@ -0,0 +1,11 @@
+/*
+ * Exercise 4.
+ *
+ * How do you access set_default from class Date from namespace 
+ * Chrono (§ 10.3.2)? Give at least three different ways.
+ *
+ * Date::set_default
+ * namespace Date { set_default }
+ * using Date::set_default; set_default
+ *
+ */

541dbc28c09b2c5389b58b56eaa37547fac57d48
yukipyon <wolfvbear@gmail.com>
2014-03-10 18:36:55 +0000

    Exercise 4.

---
+++ The C++ Programming Language Special Edition (2000)/chapter:15/exercise04.cpp
@@ -0,0 +1,15 @@
+/*
+ * Exercise 4.
+ *
+ * Write a standards-conforming c++ program containing a sequence of at least 
+ * ten different consecutive keywords not separated by identifiers, operators, 
+ * punctuation characters, etc.
+ *
+ */
+
+struct a {
+
+	virtual inline const unsigned long long int b();
+};
+
+int main() {}

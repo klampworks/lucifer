74f46b41dd2b2fee47f2dc5f6e1e0f42a528c6f7
yukipyon <wolfvbear@gmail.com>
2014-03-15 15:21:35 +0000

    Exercise 4.

---
+++ The C++ Programming Language Special Edition (2000)/chapter:20/exercise04.cpp
@@ -0,0 +1,12 @@
+/*
+ * Exercise 4.
+ *
+ * Class basic_string has a lot of members. Which could be made nonmember 
+ * functions without loss of efficiency or notational convenience?
+ *
+ */
+
+compare
+substr
+find
+rfind...

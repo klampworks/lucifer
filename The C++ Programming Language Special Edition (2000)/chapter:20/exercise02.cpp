43a0122e74396118ece70436fa6c0041635478bd
yukipyon <wolfvbear@gmail.com>
2014-03-15 15:08:16 +0000

    Exercise 2.

---
+++ The C++ Programming Language Special Edition (2000)/chapter:20/exercise02.cpp
@@ -0,0 +1,14 @@
+/*
+ * Exercise 2.
+ *
+ * Make a list of differences between vector and basic_string. Which 
+ * differences are impor-tant?
+ *
+ */
+
+#if 0
+string only accepts templates of type "char" which cannot have a user defined
+copy constructor.
+
+string has no end or begin methods.
+#endif

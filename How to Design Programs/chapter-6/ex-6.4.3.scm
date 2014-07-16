784ab32635b3ab1f4a61ce3f1db2f5b2fe929a7a
yukipyon <wolfvbear@gmail.com>
2014-07-16 21:17:25 +0100

    Exercise 6.4.3

---
+++ How to Design Programs/chapter-6/ex-6.4.3.scm
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex-6.4.3) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "draw.rkt" "teachpack" "htdp")))))
+; Where word =~ m/[a-z]{3}/
+(define-struct tlw (word))

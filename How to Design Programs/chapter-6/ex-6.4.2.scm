baa33a3b11cf513eaed80e228bb5fb956f84f9e1
yukipyon <wolfvbear@gmail.com>
2014-07-16 21:17:21 +0100

    Exercise 6.4.2

---
+++ How to Design Programs/chapter-6/ex-6.4.2.scm
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex-6.4.2) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "draw.rkt" "teachpack" "htdp")))))
+; Where hours minutes and seconds are numbers.
+(define-struct pit (hours minutes seconds))

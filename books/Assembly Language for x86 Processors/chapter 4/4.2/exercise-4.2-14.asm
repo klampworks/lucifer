5435c8e4e53284033f49c7ecf81154c76dbc4859
yukipyon <wolfvbear@gmail.com>
2014-11-30 22:16:00 +0000

    Exercise 4.2-14.

---
+++ books/Assembly Language for x86 Processors/chapter 4/4.2/exercise-4.2-14.asm
@@ -0,0 +1,19 @@
+INCLUDE c:\Irvine\Irvine32.INC
+
+.data 
+	val1 BYTE 10h
+	val2 WORD 8000h
+	val3 DWORD 0ffffh
+	val4 WORD 7fffh
+
+.code
+main PROC
+	xor eax, eax
+	mov al, 4
+	mov bl, 3
+	neg bl
+	add al, bl
+	call DumpRegs
+	exit
+main ENDP
+END main

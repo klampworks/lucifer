f1939d332afce73d79b4d5c95feddf9341cc7ce2
yukipyon <wolfvbear@gmail.com>
2015-01-13 22:10:18 +0000

    Exercise 3-4.

---
+++ books/Programming in Haskell/Chapter 03/exercise04.hs
@@ -0,0 +1,12 @@
+{-
+ - Exercise 3-4.
+ -
+ - Why is it not feasible in general for function types to be instances 
+ - of the Eq class? When is it feasible? Hint: two functions of the same type 
+ - are equal if they always return equal results for equal arguments.
+ -}
+
+ {-
+  - † How can the compiler decide if two functions return the same value
+  - without executing the code? What if the functions do not return at all?
+  -}

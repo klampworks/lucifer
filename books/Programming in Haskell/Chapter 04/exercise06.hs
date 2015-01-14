b914ade596de1d1ac25d9576aeff5f4d50188d03
yukipyon <wolfvbear@gmail.com>
2015-01-14 21:58:22 +0000

    Exercise 4-6.

---
+++ books/Programming in Haskell/Chapter 04/exercise06.hs
@@ -0,0 +1,8 @@
+{-
+ - Exercise 4-6.
+ -
+ - Show how the curried function definition mult x y z = x * y * z 
+ - can be understood in terms of lambda expressions.
+ -}
+
+mult = \x -> (\y -> (\z -> x * y * z))

0a054af5a30f121a93c8beaeb1ce5802a9354296
yukipyon <wolfvbear@gmail.com>
2015-01-19 23:02:14 +0000

    Exercise 7-5.

---
+++ books/Programming in Haskell/Chapter 07/exercise5.hs
@@ -0,0 +1,9 @@
+{-
+ - Exercise 7-5.
+ -
+ - Explain why the following definition is invalid:
+ - sumsqreven = compose [sum, map (↑2), filter even ]
+ -
+ - Because the composition operator is associative and the order in which 
+ - the above functions are applied affects the result.
+ -}

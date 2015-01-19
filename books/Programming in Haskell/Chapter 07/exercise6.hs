d6de99c051f887fd724f0d339b72967ea11b8cd5
yukipyon <wolfvbear@gmail.com>
2015-01-19 23:02:17 +0000

    Exercise 7-6.

---
+++ books/Programming in Haskell/Chapter 07/exercise6.hs
@@ -0,0 +1,11 @@
+{-
+ - Exercuse 7-6.
+ -
+ - Without looking at the standard prelude, define the higher-order library
+ - function curry that converts a function on pairs into a curried function, and,
+ - conversely, the function uncurry that converts a curried function with two
+ - arguments into a function on pairs.
+ - Hint: first write down the types of the two functions.
+ -}
+
+-- What?

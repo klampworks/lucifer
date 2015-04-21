de41a423093b80f89c2e43b9e1225859abbe5924
yukipyon <wolfvbear@gmail.com>
2015-04-21 20:21:36 +0100

    Chapter 1 Part 3 Exercise 02 - Section 3

--- books/Practical Reverse Engineering/Chapter 1/part 3/exercise02.c
+++ books/Practical Reverse Engineering/Chapter 1/part 3/exercise02.c
@@ -6,7 +6,9 @@ struct idtr_t {
 
 BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
+    HANDLE h;
     struct LPPROCESSENTRY32 p;
+
 
     struct idtr_t idtr;
     __sidt(&idtr);
@@ -14,7 +16,32 @@ BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     if (idtr.base < 0x8003F400 && idtr.base > 0x80047400)
         return 0; 
 
+    h = 0;
+    memset(&p, 0, 0x49);
     
+    handle = CreateToolhelp32Snapshot(2, 0);
+    if (handle == -1)
+        return 0;
+
+    if (Process32First(h, &p) != 0) {
+        if (stricmp(p.szExeFile, unknown_string) != 0) {
+
+            s:
+            if (Process32Next(h, &p) != 0) {
+                if (stricmp(p.szExeFile, unknown_string) != 0) {
+                    goto s;
+                } else {
+                    goto strmatch;
+                }
+            } else {
+                goto noproc;
+            }
+    } else {
+        goto noproc;
+    }
+                
+                
+           
 
 }
 

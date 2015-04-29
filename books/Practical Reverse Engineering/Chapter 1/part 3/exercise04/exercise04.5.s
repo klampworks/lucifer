240e4d495eafb2eee90d56d5f6e7e2da50150a40
yukipyon <wolfvbear@gmail.com>
2015-04-29 20:02:52 +0000

    Chapter 1 Part 3 Exercise 04 strcmp - write function.

---
+++ books/Practical Reverse Engineering/Chapter 1/part 3/exercise04/exercise04.5.s
@@ -0,0 +1,37 @@
+section .data
+str1 db "Hello",0xa,0
+len1 equ $ - str1
+str2 db "hello",0xa,0
+len2 equ $ - str2
+
+section .text
+    global _start
+
+_start:
+
+    push len1
+    push str1
+    call write_msg
+
+    push len2
+    push str2
+    call write_msg
+
+    mov ebx, eax
+    mov eax, 1
+    int 0x80
+
+write_msg:
+    push ebp
+    mov ebp, esp
+
+    mov edx, [ebp+0xc]
+    mov ecx, [ebp+8]
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov esp, ebp
+    pop ebp
+    ret 8
+    
